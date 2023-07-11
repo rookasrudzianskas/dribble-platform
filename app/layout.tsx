b10472b3b3cbcb54cb8e5835b11f7a591f00a6ca
Rokas <rookas.rudzenskas@gmail.com>
2023-07-11 16:21:33 +0300

    FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
+import Navbar from "@/components/NavBar";
 // import Footer from "@/components/Footer";
-// import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Dribble",
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
-        {/*<Navbar />*/}
+        <Navbar />
         <main>
           {children}
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,29 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      Home!
-    </main>
+    <section className="flexStart flex-col paddings mb-16">
+      {/*<Categories />*/}
+
+      {/*<section className="projects-grid">*/}
+      {/*  {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (*/}
+      {/*    <ProjectCard*/}
+      {/*      key={`${node?.id}`}*/}
+      {/*      id={node?.id}*/}
+      {/*      image={node?.image}*/}
+      {/*      title={node?.title}*/}
+      {/*      name={node?.createdBy.name}*/}
+      {/*      avatarUrl={node?.createdBy.avatarUrl}*/}
+      {/*      userId={node?.createdBy.id}*/}
+      {/*    />*/}
+      {/*  ))}*/}
+      {/*</section>*/}
+
+      {/*<LoadMore*/}
+      {/*  startCursor={data?.projectSearch?.pageInfo?.startCursor}*/}
+      {/*  endCursor={data?.projectSearch?.pageInfo?.endCursor}*/}
+      {/*  hasPreviousPage={data?.projectSearch?.pageInfo?.hasPreviousPage}*/}
+      {/*  hasNextPage={data?.projectSearch?.pageInfo.hasNextPage}*/}
+      {/*/>*/}
+    </section>
   )
 }
