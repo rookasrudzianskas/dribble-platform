9f5ada4facafe6559459df8f1b919f5835723641
Rokas <rookas.rudzenskas@gmail.com>
2023-07-12 11:00:46 +0300

    FIX

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { useRouter } from 'next/navigation';
 
-// import FormField from './FormField';
+import FormField from './FormField';
 import Button from './Button';
-// import CustomMenu from './CustomMenu';
+import CustomMenu from './CustomMenu';
 import { categoryFilters } from '@/constant';
 import { updateProject, createNewProject, fetchToken } from '@/lib/actions';
 import { FormState, ProjectInterface, SessionInterface } from '@/common.types';
@@ -27,12 +27,136 @@ const ProjectForm = ({ type, session, project }: Props) => {
     liveSiteUrl: project?.liveSiteUrl || "",
     githubUrl: project?.githubUrl || "",
     category: project?.category || ""
-  })
+  });
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes('image')) {
+      alert('Please upload an image!');
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result)
+    };
+  };
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setSubmitting(true)
+
+    const { token } = await fetchToken()
+
+    try {
+      if (type === "create") {
+        await createNewProject(form, session?.user?.id, token)
+
+        router.push("/")
+      }
+
+      if (type === "edit") {
+        await updateProject(form, project?.id as string, token)
+
+        router.push("/")
+      }
+
+    } catch (error) {
+      alert(`Failed to ${type === "create" ? "create" : "edit"} a project. Try again!`);
+    } finally {
+      setSubmitting(false)
+    }
+  }
 
   return (
-    <div>
+    <form
+      onSubmit={handleFormSubmit}
+      className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && 'Choose a poster for your project'}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept='image/*'
+          required={type === "create" ? true : false}
+          className="form_image-input"
+          onChange={(e) => handleChangeImage(e)}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20" alt="image"
+            fill
+          />
+        )}
+      </div>
 
-    </div>
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+
+      <FormField
+        title='Description'
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects."
+        isTextArea
+        setState={(value) => handleStateChange('description', value)}
+      />
+
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://byrookas.com"
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+
+      <FormField
+        type="url"
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/rookasrudzianskas"
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+
+      <div className="flexStart w-full">
+        <Button
+          title={submitting ? `${type === "create" ? "Creating" : "Editing"}` : `${type === "create" ? "Create" : "Edit"}`}
+          type="submit"
+          leftIcon={submitting ? "" : "/plus.svg"}
+          submitting={submitting}
+        />
+      </div>
+
+    </form>
   );
 };
 
